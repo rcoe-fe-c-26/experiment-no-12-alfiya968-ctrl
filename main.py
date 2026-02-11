eb171c3cf4833c568c2bab0aca9742496714fc59
alfiya968-ctrl <alfiya968@eng.rizvi.edu.in>
2026-02-11 21:19:32 +0530

    Modify comments with coder name and date Exp12
    Updated coder name and date in comments.

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 # AIM: Develop a Python program that reads a text file and 
 # prints words of specified lengths (e.g., three, four, 
 # five, etc.) found within the file.
-# Coder:
-# Date:
+# Coder: Alfiya Rizvi
+# Date: 10-2-26
 
-print("--- Extracting Words from Text File ---\n")
+
+
+print("--- Extracting Words from Text File ---")
+
+length = int(input("Enter Length of Words: "))
+
+
+with open("story.txt", "r") as file:
+    text = file.read()
+
+
+import string
+
+for ch in string.punctuation:
+    text = text.replace(ch, " ")
+
+words = text.split()
+
+
+filtered = [w.lower() for w in words if len(w) == length]
+
+unique_words = sorted(set(filtered))
+
+print(f"\nWords with length {length} are: {unique_words}")
 
